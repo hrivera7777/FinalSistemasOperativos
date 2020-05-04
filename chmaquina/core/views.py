7818cfe2be3967dc6474e6a588cde6860db16ac6
Hans Rivera <hrivera@unal.edu.co>
2020-05-04 00:15:34 -0500

    se generan las posiciones de memoria en el front

--- chmaquina/core/views.py
+++ chmaquina/core/views.py
@@ -11,7 +11,12 @@ from .ejecucion import ejecucion
 
 
 
+"""
 
+esto es update view cuidado
+
+"""
+#################################################################################################################3
 class HomePageView(UpdateView):
     #form_class = ArchivoForm
     #model = ArchivosCh se cambió el modelo para poder recuperar la memoria y el kernel
@@ -28,6 +33,7 @@ class HomePageView(UpdateView):
         #nombre = ruta.name
         tup = EjecArchCh.objects.all()
         nombre=""
+        memorias=[]
         for tp in tup:
             nombres=tp.archivo
             tempo2= str(nombres).split('/')
@@ -47,7 +53,11 @@ class HomePageView(UpdateView):
         profile, created= EjecArchCh.objects.get_or_create()
         return profile
     
-# Create your views here.
+# ########################################################################################################################
+
+
+
+
 ## con esta view permite agregar varios archivos ch a la vez 
 class HomePageView2(CreateView):
     #form_class = ArchivoForm
@@ -66,12 +76,27 @@ class HomePageView2(CreateView):
         tup = EjecArchCh.objects.all()
         nombre=""
         for tp in tup:
-            nombres=tp.archivo
+            nombres=tp.archivo # aquí se toman todas la rutas de los archivos cargados en la bd
+            memorias= tp.memoria # aquí se toman las cantidades de memoria guardadas en la bd
+            kernels=tp.kernel # aquí se toman las cantidades de kernel guardadas en la bd
             tempo2= str(nombres).split('/')
             nombre=tempo2[1]
-        instancia= sintax()
+            memoriaFinal=int(memorias) # con esto sabemos cuanto es la memoria final entregado por el usuario
+            kernelFinal=int(kernels) # con esto sabemos cuanto es el kernel final entregado por el usuario
+        
+        tamMemoriaDisp = memoriaFinal- kernelFinal -1 # aquí se verifica cuanta memoria disponible hay (kernel - acumulador - total memoria)
+        cantidadKernel=[] # se utilizan listas para mostrar las posiciones de memoria en el kernel 
+        cantidMemoriaDisp=[] # se utulizan listas para mostrar las posiciones de memoria disponible  
+        
+        for i in range (tamMemoriaDisp): #aqui se llena la lista con los valores de la posicion de memoria disponible
+            cantidMemoriaDisp.append(i+kernelFinal+1) 
+        
+        for i in range (kernelFinal): #aqui se llena la lista con los valores de la posicion de memoria que ocupa el kernel
+            cantidadKernel.append(i+1) 
+
+        instancia= sintax() # se crea una instancia de la clase sintax para poder llamar el método que prueba toda la sintaxis de un archivo .ch
         #print(sintax.abrirArchivo(self))    
-        return render(request, self.template_name,{'title': "Ch Máquina",'nombre':nombre, 'sintax':instancia.pruebaTotal()}) # })#,
+        return render(request, self.template_name,{'title': "Ch Máquina",'nombre':nombre, 'sintax':instancia.pruebaTotal(),'memoriaFinal': cantidMemoriaDisp, 'kernel': cantidadKernel}) # })#,
         #'sintax':instancia.abrirArchivo()
     """
     def get_object(self, queryset=None):
@@ -84,5 +109,4 @@ class HomePageView2(CreateView):
         profile, created= EjecArchCh.objects.get_or_create()
         return profile
     
-# Create your views here.
 
