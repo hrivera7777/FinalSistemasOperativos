2a1aff0fb0b59508b212c3aefadddf46fafdfb6d
Hans Rivera <hrivera@unal.edu.co>
2020-06-30 17:07:56 -0500

    Se agrega parcialmete SRTN

--- chmaquina/core/urls.py
+++ chmaquina/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HomePageView2, salirView, InicioPageView,SPNView, PrioridadNoExpView
+from .views import HomePageView2, salirView, InicioPageView,SPNView, PrioridadNoExpView, SRTNView
 
 urlpatterns = [
     path('', InicioPageView.as_view(), name="home"),
     path('fcfs/', HomePageView2.as_view(), name="fcfs"),
     path('spn/', SPNView.as_view(), name="spn"),
+    path('srtn/', SRTNView.as_view(), name="srtn"),
     path('prioridadNoExp/', PrioridadNoExpView.as_view(), name="prioridadNoExp"),
     path('delete/', salirView.as_view(), name="salir"),
     
