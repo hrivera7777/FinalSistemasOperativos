b3644d8cda8f1d67ac0e3690304b672d5d52c49e
Hans Rivera <hrivera@unal.edu.co>
2020-07-03 16:16:27 -0500

    Se agrega prioridad exporpiativo

--- chmaquina/core/urls.py
+++ chmaquina/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView2, salirView, InicioPageView,SPNView, PrioridadNoExpView, SRTNView, RRView
+from .views import HomePageView2, salirView, InicioPageView,SPNView, PrioridadNoExpView, SRTNView, PrioridadExpView ,RRView
 
 urlpatterns = [
     path('', InicioPageView.as_view(), name="home"),
@@ -7,6 +7,7 @@ urlpatterns = [
     path('spn/', SPNView.as_view(), name="spn"),
     path('srtn/', SRTNView.as_view(), name="srtn"),
     path('prioridadNoExp/', PrioridadNoExpView.as_view(), name="prioridadNoExp"),
+    path('prioridadExp/', PrioridadExpView.as_view(), name="prioridadExp"),
     path('rr/', RRView.as_view(), name="rr"),
     path('delete/', salirView.as_view(), name="salir"),
     
